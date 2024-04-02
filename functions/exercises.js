c4002f0837ef09e161a2abdd59896c1c6c0631c7
Your Name <108307350+pjduri@users.noreply.github.com>
2024-04-02 12:16:32 -0500

    funcsercises bonus

--- functions/exercises.js
+++ functions/exercises.js
@@ -1,59 +1,59 @@
-function makeLine(size, char) {
+function makeLine(size, char = '#') {
     let hashLine = ''
     for (let i=0; i<size; i++) {
         hashLine += char
     }
     return hashLine
 }
-console.log(makeLine(5, '$'))
+// console.log(makeLine(5, '$'))
 
-function makeSquare(size) {
-    return makeRectangle(size, size)
+function makeSquare(size, char = '#') {
+    return makeRectangle(size, size, char)
 }
 // console.log(makeSquare(5))
 
-function makeRectangle(length, height) {
+function makeRectangle(length, height, char = '#') {
     let rectangle = ''
     for (let i=0; i<height; i++) {
-        rectangle += makeLine(length) + '\n'
+        rectangle += makeLine(length, char) + '\n'
     }
     return rectangle.slice(0, -1)
 }
 // console.log(makeRectangle(9, 4))
 
-function makeDownwardStairs(height) {
+function makeDownwardStairs(height, char = '#') {
     let triangle = ''
     for (let i=0; i<height; i++) {
-        triangle += makeLine(i+1) + '\n'
+        triangle += makeLine(i+1, char) + '\n'
     }
     return triangle.slice(0, -1)
 }
-// console.log(makeDownwardStairs(7))
+// console.log(makeDownwardStairs(7, '$'))
 
-function makeSpaceLine(numSpaces, numChars) {
+function makeSpaceLine(numSpaces, numChars, char = '#') {
     let spaceLine = ''
     for (let i=0; i<numSpaces; i++) {
         spaceLine += ' '
     }
-    spaceLine += makeLine(numChars)
+    spaceLine += makeLine(numChars, char)
     for (let i=0; i<numSpaces; i++) {
         spaceLine += ' '
     }
     return spaceLine
 }
-// console.log(makeSpaceLine(4, 7))
+// console.log(makeSpaceLine(4, 7, 'O'))
 
-function makeIsoscelesTriangle(height) {
+function makeIsoscelesTriangle(height, char = '#') {
     let triangle = ''
     for (let i=0; i<height; i++) {
-        triangle += makeSpaceLine(height - i - 1, i * 2 + 1) + '\n'
+        triangle += makeSpaceLine(height - i - 1, i * 2 + 1, char) + '\n'
     }
     return triangle.slice(0, -1)
 }
-// console.log(makeIsoscelesTriangle(9))
+// console.log(makeIsoscelesTriangle(9, '?'))
 
-function makeDiamond(height) {
-    let triangle = makeIsoscelesTriangle(height)
+function makeDiamond(height, char = '#') {
+    let triangle = makeIsoscelesTriangle(height, char)
     let bottomHalf = '\n' + triangle.split('').reverse().join('')
     return triangle + bottomHalf
 }
