59d63e1773ed352da76158585f11c7afdab28f0f
Your Name <108307350+pjduri@users.noreply.github.com>
2024-04-08 10:13:15 -0500

    more func studio

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -46,10 +46,10 @@ let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
-// console.log(sortArray(nums1));
-// console.log(sortArray(nums2));
-// console.log(sortArray(nums3));
+console.log(sortArray(nums1));
+console.log(sortArray(nums2));
+console.log(sortArray(nums3));
 
-console.log(recursiveSorter(nums1));
-console.log(recursiveSorter(nums2));
-console.log(recursiveSorter(nums3));
+// console.log(recursiveSorter(nums1));
+// console.log(recursiveSorter(nums2));
+// console.log(recursiveSorter(nums3));
