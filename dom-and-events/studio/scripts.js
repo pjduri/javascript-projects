d9e5ad6950dfb0590640a93ad003291da8585119
Your Name <108307350+pjduri@users.noreply.github.com>
2024-04-23 15:21:47 -0500

    first step mob code

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,34 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', function() {
+
+    const takeOff = document.getElementById('takeoff');
+    const spaceShuttleHeight = document.getElementById('spaceShuttleHeight');
+    const flightStatus = document.getElementById('flightStatus');
+    const shuttleBackground = document.getElementById('shuttleBackground');
+    
+    const rocket = document.getElementById('rocket');
+    rocket.style.position = 'absolute';
+    rocket.style.bottom = '0px';
+    rocket.style.left = '0px';
+
+    takeOff.addEventListener('click', () => {
+        const confirmed = confirm('Are you sure the shuttle is ready for takeof?');
+        if(confirmed) {
+            flightStatus.innerHTML = 'Shuttle in flight.';
+            spaceShuttleHeight.innerHTML = 10000;
+            shuttleBackground.style.backgroundColor = 'blue';
+            rocket.style.bottom = '10px';
+            rocket.style.left = '10px';
+        }
+    })
+
+
+
+
+
+
+
+
+
+})
